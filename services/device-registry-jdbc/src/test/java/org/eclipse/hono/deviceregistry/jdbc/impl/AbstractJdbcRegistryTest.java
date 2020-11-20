68a47ff906e6569d3415ce18ff8d5e697691b21f
Lari Hotari <lari@hotari.net>
2020-11-20 19:31:26 +0200

    Add possibility to test MS SQLServer with Testcontainers
    Signed-off-by: Lari Hotari <lari@hotari.net>

--- services/device-registry-jdbc/src/test/java/org/eclipse/hono/deviceregistry/jdbc/impl/AbstractJdbcRegistryTest.java
+++ services/device-registry-jdbc/src/test/java/org/eclipse/hono/deviceregistry/jdbc/impl/AbstractJdbcRegistryTest.java
@@ -14,12 +14,14 @@
 package org.eclipse.hono.deviceregistry.jdbc.impl;
 
 import java.io.IOException;
+import java.io.StringReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.sql.DriverManager;
 import java.sql.SQLException;
 import java.util.Optional;
-import java.util.Properties;
 import java.util.UUID;
+import java.util.concurrent.atomic.AtomicLong;
 
 import org.eclipse.hono.auth.SpringBasedHonoPasswordEncoder;
 import org.eclipse.hono.deviceregistry.jdbc.config.DeviceServiceProperties;
@@ -44,14 +46,22 @@ import io.opentracing.noop.NoopSpan;
 import io.opentracing.noop.NoopTracerFactory;
 import io.vertx.core.Vertx;
 import io.vertx.junit5.VertxExtension;
+import org.testcontainers.containers.MSSQLServerContainer;
 
 @ExtendWith(VertxExtension.class)
 abstract class AbstractJdbcRegistryTest {
+    enum DatabaseType {
+        H2,
+        SQLSERVER
+    }
+    private static final DatabaseType DATABASE_TYPE = DatabaseType.valueOf(System.getProperty(AbstractJdbcRegistryTest.class.getSimpleName() + ".databaseType", "H2").toUpperCase());
+    private static final MSSQLServerContainer<?> MSSQL_SERVER_CONTAINER = DATABASE_TYPE == DatabaseType.SQLSERVER ? new MSSQLServerContainer<>("mcr.microsoft.com/mssql/server:2017-CU12") : null;
+    private static final AtomicLong MSSQL_UNIQUE_ID = new AtomicLong(System.currentTimeMillis());
 
     protected static final Span SPAN = NoopSpan.INSTANCE;
 
     private static final Tracer TRACER = NoopTracerFactory.create();
-    private static final Path EXAMPLE_SQL_BASE = Path.of("..", "base-jdbc", "src", "main", "sql", "h2");
+    private static final Path EXAMPLE_SQL_BASE = Path.of("..", "base-jdbc", "src", "main", "sql", DATABASE_TYPE.name().toLowerCase());
 
     private static final Path BASE_DIR = Path.of("target/data").toAbsolutePath();
 
@@ -65,20 +75,15 @@ abstract class AbstractJdbcRegistryTest {
 
     @BeforeEach
     void startDevices(final Vertx vertx) throws IOException, SQLException {
-        final var database = UUID.randomUUID().toString();
-        final var url = "jdbc:h2:" + BASE_DIR.resolve(database).toAbsolutePath().toString();
+        final var jdbc = resolveJdbcProperties();
 
         try (
-                var connection = new Driver().connect(url, new Properties());
+                var connection = DriverManager.getConnection(jdbc.getUrl(), jdbc.getUsername(), jdbc.getPassword());
                 var script = Files.newBufferedReader(EXAMPLE_SQL_BASE.resolve("create.devices.sql"))
         ) {
             // pre-create database
             RunScript.execute(connection, script);
         }
-
-        final var jdbc = new JdbcProperties();
-        jdbc.setDriverClass(Driver.class.getName());
-        jdbc.setUrl(url);
 
         final var properties = new DeviceServiceProperties();
 
@@ -103,22 +108,61 @@ abstract class AbstractJdbcRegistryTest {
 
     }
 
+    private JdbcProperties resolveJdbcProperties() {
+        final var jdbc = new JdbcProperties();
+        switch (DATABASE_TYPE) {
+            case H2:
+                jdbc.setDriverClass(Driver.class.getName());
+                jdbc.setUrl("jdbc:h2:" + BASE_DIR.resolve(UUID.randomUUID().toString()).toAbsolutePath());
+                break;
+            case SQLSERVER:
+                MSSQL_SERVER_CONTAINER.start();
+                jdbc.setDriverClass("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+                jdbc.setUrl(MSSQL_SERVER_CONTAINER.getJdbcUrl());
+                jdbc.setUsername(MSSQL_SERVER_CONTAINER.getUsername());
+                jdbc.setPassword(MSSQL_SERVER_CONTAINER.getPassword());
+                createNewPerTestSchemaAndUser(jdbc);
+                break;
+            default:
+                throw new UnsupportedOperationException(DATABASE_TYPE.name() + " is not supported.");
+        }
+        try {
+            Class.forName(jdbc.getDriverClass());
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException("Cannot load JDBC driver class", e);
+        }
+        return jdbc;
+    }
+
+    void createNewPerTestSchemaAndUser(JdbcProperties jdbc) {
+        String schemaName = "test" + MSSQL_UNIQUE_ID.incrementAndGet();
+        String userName = "user" + MSSQL_UNIQUE_ID.incrementAndGet();
+        String sql = "create login " + userName + " with password='" + jdbc.getPassword() + "';\n" +
+                "create schema " + schemaName + ";\n" +
+                "create user " + userName + " for login " + userName + " with default_schema = " + schemaName + ";\n" +
+                "exec sp_addrolemember 'db_owner', '" + userName + "';\n";
+        try (
+                var connection = DriverManager.getConnection(jdbc.getUrl(), jdbc.getUsername(), jdbc.getPassword());
+                var script = new StringReader(sql)
+        ) {
+            RunScript.execute(connection, script);
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        jdbc.setUsername(userName);
+    }
+
     @BeforeEach
     void startTenants(final Vertx vertx) throws IOException, SQLException {
-        final var database = UUID.randomUUID().toString();
-        final var url = "jdbc:h2:" + BASE_DIR.resolve(database).toAbsolutePath().toString();
+        final var jdbc = resolveJdbcProperties();
 
         try (
-                var connection = new Driver().connect(url, new Properties());
+                var connection = DriverManager.getConnection(jdbc.getUrl(), jdbc.getUsername(), jdbc.getPassword());
                 var script = Files.newBufferedReader(EXAMPLE_SQL_BASE.resolve("create.tenants.sql"))
         ) {
             // pre-create database
             RunScript.execute(connection, script);
         }
-
-        final var jdbc = new JdbcProperties();
-        jdbc.setDriverClass(Driver.class.getName());
-        jdbc.setUrl(url);
 
         this.tenantAdapter = new TenantServiceImpl(
                 Stores.adapterStore(vertx, TRACER, jdbc),
